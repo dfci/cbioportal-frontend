62894e75ff42f5d3b154120f2097c338281d0e82
Eric Marriott <marriott@bcb.dfci.harvard.edu>
2019-02-15 10:26:00 -0500

    url fix

--- src/pages/resultsView/bookmark/BookmarkModal.tsx
+++ src/pages/resultsView/bookmark/BookmarkModal.tsx
@@ -53,7 +53,7 @@ export class BookmarkModal extends React.Component<{ onHide: () => void, urlProm
     initializeClipboards(){
             this.clipboards.push(new Clipboard(this.sessionButton, {
                 text: function() {
-                    return this.urlData.sessionUrl || this.urlData.fullUrl;
+                    return this.urlData.sessionUrl.replace(".edu/", ".edu:8443/") || this.urlData.fullUrl;
                 }.bind(this),
                 container: this.container
             }));
