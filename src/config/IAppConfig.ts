c326e77ebc4715ecae766ac40f1cf9b9a2724726
Jeremy R. Easton-Marks <j.r.eastonmarks@gmail.com>
2024-09-24 08:47:27 -0500

    Add DownloadProfileData Link

--- src/config/IAppConfig.ts
+++ src/config/IAppConfig.ts
@@ -187,6 +187,7 @@ export interface IServerConfig {
     vaf_log_scale_default: boolean; // this has a default
     skin_study_view_show_sv_table: boolean; // this has a default
     enable_study_tags: boolean;
+    show_profile_download: boolean;
     clickhouse_mode: boolean;
     download_custom_buttons_json: string;
 }
